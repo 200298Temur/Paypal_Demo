24d8df869f74c5562c803dea643309b7b795c471
200298Temur <temuru502@gmail.com>
2024-03-25 12:48:15 +0500

    Sinov Paypal

---
+++ src/main/java/com/Temur/Paypal_Demo/PaypalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Temur.Paypal_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Temur/Paypal_Demo/PaypalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Temur.Paypal_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
